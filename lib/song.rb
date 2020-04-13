ad36ddc712fa71dc878c8701e948d3134597a265
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-13 20:43:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def initialize(name, artist, genre)
     @@count += 1 
     @@genres << genre
-    @@artists << artists
+    @@artists << artist
     @name = name
     @artist = artist
     @genre = genre
